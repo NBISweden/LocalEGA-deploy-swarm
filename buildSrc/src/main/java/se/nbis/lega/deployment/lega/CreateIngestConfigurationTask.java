b0fa7fefb7bd92660e56477fba48cfad49bdf2a8
amgadhanafy <amgad2002@gmail.com>
2018-12-19 13:23:40 +0100

    refactoring

--- buildSrc/src/main/java/se/nbis/lega/deployment/lega/CreateIngestConfigurationTask.java
+++ buildSrc/src/main/java/se/nbis/lega/deployment/lega/CreateIngestConfigurationTask.java
@@ -1,6 +1,5 @@
 package se.nbis.lega.deployment.lega;
 
-import java.io.File;
 import java.io.IOException;
 import org.gradle.api.tasks.TaskAction;
 import se.nbis.lega.deployment.LegaPrivateTask;
@@ -15,9 +14,8 @@ public class CreateIngestConfigurationTask extends LegaPrivateTask {
 
     @TaskAction
     public void run() throws IOException {
-        File traceFile = getProject().file(LEGA_PRIVATE_TMP_TRACE);
-        writeTrace(S3_ACCESS_KEY, readTrace(traceFile, S3_ACCESS_KEY));
-        writeTrace(S3_SECRET_KEY, readTrace(traceFile, S3_SECRET_KEY));
+        writeTrace(S3_ACCESS_KEY, readTrace(S3_ACCESS_KEY));
+        writeTrace(S3_SECRET_KEY, readTrace(S3_SECRET_KEY));
     }
 
 }
