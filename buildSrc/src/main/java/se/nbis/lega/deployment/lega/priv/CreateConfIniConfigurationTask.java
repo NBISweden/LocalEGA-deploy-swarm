c22b2bda04d67f112e0ca0482c4c53fea63c5d42
Dmytro Titov <dmytro.titov@gmail.com>
2019-01-23 11:51:37 +0100

    Make Ingestion microservice use S3 service as a back-end.

--- buildSrc/src/main/java/se/nbis/lega/deployment/lega/priv/CreateConfIniConfigurationTask.java
+++ buildSrc/src/main/java/se/nbis/lega/deployment/lega/priv/CreateConfIniConfigurationTask.java
@@ -9,33 +9,34 @@ import java.nio.charset.Charset;
 
 public class CreateConfIniConfigurationTask extends LegaPrivateTask {
 
-    @TaskAction
-    public void run() throws IOException {
+    @TaskAction public void run() throws IOException {
         generateConfIni();
         createConfig(Config.CONF_INI.getName(), getProject().file(".tmp/conf.ini"));
     }
 
     private void generateConfIni() throws IOException {
-        String s3AccessKey = readTrace(VAULT_S3_ACCESS_KEY);
-        String s3SecretKey = readTrace(VAULT_S3_SECRET_KEY);
+        String inboxS3AccessKey = readTrace(INBOX_S3_ACCESS_KEY);
+        String inboxS3SecretKey = readTrace(INBOX_S3_SECRET_KEY);
+        String vaultS3AccessKey = readTrace(VAULT_S3_ACCESS_KEY);
+        String vaultS3SecretKey = readTrace(VAULT_S3_SECRET_KEY);
         String postgresPassword = readTrace(DB_LEGA_IN_PASSWORD);
         File confIni = getProject().file(".tmp/conf.ini");
         FileUtils.write(confIni, String.format(
-            "[DEFAULT]\n" + "log = console\n" + System.lineSeparator() + "[keyserver]\n"
-                + "port = 8080\n" + System.lineSeparator() + "[quality_control]\n"
+            "[DEFAULT]\n" + "log = console\n" + "\n" + "[keyserver]\n" + "port = 8080\n" + "\n"
+                + "[quality_control]\n"
                 + "keyserver_endpoint = http://keys:8080/keys/retrieve/%%s/private/bin?idFormat=hex\n"
-                + System.lineSeparator() + "[inbox]\n" + "location = /ega/inbox/%%s\n"
-                + "mode = 2750\n" + System.lineSeparator() + "[vault]\n" + "driver = S3Storage\n"
-                + "url = http://vault-s3:9000\n" + "access_key = %s\n" + "secret_key = %s\n"
-                + "#region = lega\n" + System.lineSeparator() + System.lineSeparator()
+                + "\n" + "[inbox]\n" + "driver = S3Storage\n" + "url = http://inbox-s3:9000\n"
+                + "access_key = %s\n" + "secret_key = %s\n" + "#region = lega\n" + "\n"
+                + "[vault]\n" + "driver = S3Storage\n" + "url = http://vault-s3:9000\n"
+                + "access_key = %s\n" + "secret_key = %s\n" + "#region = lega\n" + "\n"
                 + "[outgestion]\n" + "# Just for test\n"
                 + "keyserver_endpoint = http://keys:8080/keys/retrieve/%%s/private/bin?idFormat=hex\n"
-                + System.lineSeparator() + "## Connecting to Local EGA\n" + "[broker]\n"
-                + "host = mq\n" + "connection_attempts = 30\n" + "# delay in seconds\n"
-                + "retry_delay = 10\n" + System.lineSeparator() + "[postgres]\n" + "host = db\n"
-                + "port = 5432\n" + "user = lega_in\n" + "password = %s\n" + "database = lega\n"
-                + "try = 30\n" + "sslmode = require\n" + System.lineSeparator() + "[eureka]\n"
-                + "endpoint = http://cega-eureka:8761", s3AccessKey, s3SecretKey, postgresPassword),
+                + "\n" + "## Connecting to Local EGA\n" + "[broker]\n" + "host = mq\n"
+                + "connection_attempts = 30\n" + "# delay in seconds\n" + "retry_delay = 10\n"
+                + "\n" + "[postgres]\n" + "host = db\n" + "port = 5432\n" + "user = lega_in\n"
+                + "password = %s\n" + "database = lega\n" + "try = 30\n" + "sslmode = require\n"
+                + "\n" + "[eureka]\n" + "endpoint = http://cega-eureka:8761", inboxS3AccessKey,
+            inboxS3SecretKey, vaultS3AccessKey, vaultS3SecretKey, postgresPassword),
             Charset.defaultCharset());
     }
 
