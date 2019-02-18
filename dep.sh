7b0fa24f92e7c05e5158b6d62f1aca8d9415b225
Amgad Hanafy <amgad2002@gmail.com>
2019-02-18 14:42:15 +0100

    cleanup

--- dep.sh
+++
@@ -1,7 +0,0 @@
-gradle clean all
-rm -rf repo
-
-mkdir repo
-git clone --single-branch --branch $env.BRANCH_NAME https://github.com/NBISweden/LocalEGA-deploy-swarm.git repo
-gradle bootstrap
-gradle deployPrivate
