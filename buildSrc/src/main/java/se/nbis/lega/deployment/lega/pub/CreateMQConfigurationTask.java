96cd892aa46266c38db37c8a27d2221b874e4638
Amgad Hanafy <amgad2002@gmail.com>
2019-03-01 14:37:23 +0100

    load cega connection correctly

--- buildSrc/src/main/java/se/nbis/lega/deployment/lega/pub/CreateMQConfigurationTask.java
+++ buildSrc/src/main/java/se/nbis/lega/deployment/lega/pub/CreateMQConfigurationTask.java
@@ -12,21 +12,19 @@ public class CreateMQConfigurationTask extends LegaPublicTask {
         createConfig(Config.RABBITMQ_CONFIG.getName(), getProject().file("mq/rabbitmq.config"));
         createConfig(Config.ENTRYPOINT_SH.getName(), getProject().file("mq/entrypoint.sh"));
         String cegaMQPassword = readTrace(getProject().getParent().file(CEGA_TMP_TRACE), "CEGA_MQ_PASSWORD");
+        String host = getProperty(CEGA_IP);
         if (cegaMQPassword != null) {
-            String host = getProperty(CEGA_IP);
             if (host == null) {
                 host = getMachineIPAddress(Machine.CEGA.getName());
             }
-            if (getProperty(TEST_CEGA) == null) {
-                writeTrace(CEGA_CONNECTION, String.format("amqp://lega:%s@%s:5672/lega", cegaMQPassword, host));
-            } else {
-                String user = "norway1";
-                String password = "uaLu%H^R4qbF4QT_";
-                String port = "5271";
-                String vhost = "norway1";
-                host = "hellgate.crg.eu";
-                writeTrace(CEGA_CONNECTION, String.format("amqp://%s:%s@%s:%s/%s", user, password, host, port, vhost));
-            }
+            writeTrace(CEGA_CONNECTION, String.format("amqp://lega:%s@%s:5672/lega", cegaMQPassword, host));
+        } else if (getProperty(TEST_CEGA) != null) {
+            String user = "norway1";
+            String password = "uaLu%H^R4qbF4QT_";
+            String port = "5271";
+            String vhost = "norway1";
+            host = "hellgate.crg.eu";
+            writeTrace(CEGA_CONNECTION, String.format("amqp://%s:%s@%s:%s/%s", user, password, host, port, vhost));
         }
     }
 
