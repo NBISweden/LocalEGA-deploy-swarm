2743aa1fc93fafcae810450ec092e9505dcaf5a9
Dmytro Titov <dmytro.titov@gmail.com>
2019-03-06 12:10:52 +0100

    Use uiobmi/localega-broker:public and uiobmi/localega-broker:private

--- buildSrc/src/main/java/se/nbis/lega/deployment/lega/pub/Config.java
+++ buildSrc/src/main/java/se/nbis/lega/deployment/lega/pub/Config.java
@@ -2,9 +2,7 @@ package se.nbis.lega.deployment.lega.pub;
 
 public enum Config {
 
-    DEFS_JSON("lega-public.defs.json"),
-    RABBITMQ_CONFIG("lega-public.rabbitmq.config"),
-    ENTRYPOINT_SH("lega-public.entrypoint.sh");
+    ;
 
     private String name;
 
