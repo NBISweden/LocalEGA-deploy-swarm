c3d862fea2b6fcbb33818f6d0d737d170e50b033
Dmytro Titov <dmytro.titov@gmail.com>
2019-03-26 12:16:39 +0100

    Use externalized DB image

--- buildSrc/src/main/java/se/nbis/lega/deployment/lega/priv/Config.java
+++ buildSrc/src/main/java/se/nbis/lega/deployment/lega/priv/Config.java
@@ -2,13 +2,6 @@ package se.nbis.lega.deployment.lega.priv;
 
 public enum Config {
 
-    POSTGRESQL_CONF("postgresql.conf"),
-    MAIN_SQL("main.sql"),
-    GRANTS_SQL("grants.sql"),
-    DOWNLOAD_SQL("download.sql"),
-    EBI_SQL("ebi.sql"),
-    QC_SQL("qc.sql"),
-    DB_ENTRYPOINT_SH("db.entrypoint.sh"),
     SSL_CERT("ssl.cert"),
     SSL_KEY("ssl.key"),
     EGA_SEC("ega.sec"),
--- buildSrc/src/main/java/se/nbis/lega/deployment/lega/priv/CreateDBConfigurationTask.java
+++ buildSrc/src/main/java/se/nbis/lega/deployment/lega/priv/CreateDBConfigurationTask.java
@@ -9,13 +9,6 @@ public class CreateDBConfigurationTask extends LegaPrivateTask {
 
     @TaskAction
     public void run() throws IOException {
-        createConfig(Config.POSTGRESQL_CONF.getName(), getProject().file("db/postgresql.conf"));
-        createConfig(Config.MAIN_SQL.getName(), getProject().file("db/main.sql"));
-        createConfig(Config.GRANTS_SQL.getName(), getProject().file("db/grants.sql"));
-        createConfig(Config.DOWNLOAD_SQL.getName(), getProject().file("db/download.sql"));
-        createConfig(Config.EBI_SQL.getName(), getProject().file("db/ebi.sql"));
-        createConfig(Config.QC_SQL.getName(), getProject().file("db/qc.sql"));
-        createConfig(Config.DB_ENTRYPOINT_SH.getName(), getProject().file("db/entrypoint.sh"));
         writeTrace(DB_LEGA_IN_PASSWORD, UUID.randomUUID().toString().replace("-", ""));
         writeTrace(DB_LEGA_OUT_PASSWORD, UUID.randomUUID().toString().replace("-", ""));
     }
