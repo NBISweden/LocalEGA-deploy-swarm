363473c03278cf0d6696d50b230a0fa8a701a914
Dmytro Titov <dmytro.titov@gmail.com>
2019-05-21 23:24:49 +0200

    Add OkHttpClient to MinioClient

--- buildSrc/src/main/java/se/nbis/lega/deployment/test/IngestFileTask.java
+++ buildSrc/src/main/java/se/nbis/lega/deployment/test/IngestFileTask.java
@@ -1,32 +1,33 @@
 package se.nbis.lega.deployment.test;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.util.UUID;
-import java.util.concurrent.TimeoutException;
-import org.apache.commons.collections4.IterableUtils;
-import org.gradle.api.GradleException;
-import org.gradle.api.tasks.InputFile;
-import org.gradle.api.tasks.TaskAction;
-import org.xmlpull.v1.XmlPullParserException;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import io.minio.MinioClient;
-import io.minio.errors.ErrorResponseException;
-import io.minio.errors.InsufficientDataException;
-import io.minio.errors.InternalException;
-import io.minio.errors.InvalidBucketNameException;
-import io.minio.errors.InvalidEndpointException;
-import io.minio.errors.InvalidPortException;
-import io.minio.errors.NoResponseException;
+import okhttp3.OkHttpClient;
+import org.apache.commons.collections4.IterableUtils;
+import org.gradle.api.GradleException;
+import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.TaskAction;
 import se.nbis.lega.deployment.cluster.Machine;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
 
 public class IngestFileTask extends TestTask {
 
@@ -36,10 +37,7 @@ public class IngestFileTask extends TestTask {
     }
 
     @TaskAction
-    public void run() throws IOException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException,
-                    TimeoutException, InvalidKeyException, XmlPullParserException, InvalidPortException,
-                    ErrorResponseException, NoResponseException, InvalidBucketNameException, InsufficientDataException,
-                    InvalidEndpointException, InternalException, InterruptedException {
+    public void run() throws Exception {
         System.out.println("Starting ingestion...");
         String host = getProperty("legaPrivateIP");
         if (host == null) {
@@ -58,8 +56,9 @@ public class IngestFileTask extends TestTask {
         System.out.println("File seems to be ingested successfully.");
     }
 
-    private void ingest() throws IOException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException,
-                    TimeoutException {
+    private void ingest()
+        throws IOException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException,
+        TimeoutException {
         String mqPassword = readTrace(getProject().file(CEGA_TMP_TRACE), CEGA_MQ_PASSWORD);
         String mqConnectionString;
         String username;
@@ -78,35 +77,57 @@ public class IngestFileTask extends TestTask {
         factory.setUri(mqConnectionString);
         Connection connectionFactory = factory.newConnection();
         Channel channel = connectionFactory.createChannel();
-        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().deliveryMode(2)
-                        .contentType("application/json").contentEncoding(StandardCharsets.UTF_8.displayName()).build();
+        AMQP.BasicProperties properties =
+            new AMQP.BasicProperties().builder().deliveryMode(2).contentType("application/json")
+                .contentEncoding(StandardCharsets.UTF_8.displayName()).build();
 
 
         String stableId = "EGAF" + UUID.randomUUID().toString().replace("-", "");
-        channel.basicPublish("localega.v1", "files", properties,
-                        String.format("{\"user\":\"%s\",\"filepath\":\"data.raw.enc\",\"stable_id\":\"%s\"}", username,
-                                        stableId).getBytes());
+        channel.basicPublish("localega.v1", "files", properties, String
+            .format("{\"user\":\"%s\",\"filepath\":\"data.raw.enc\",\"stable_id\":\"%s\"}",
+                username, stableId).getBytes());
 
         channel.close();
         connectionFactory.close();
     }
 
-    private int getFilesAmount(String host) throws XmlPullParserException, IOException, InvalidPortException,
-                    InvalidEndpointException, InsufficientDataException, NoSuchAlgorithmException, NoResponseException,
-                    InternalException, InvalidKeyException, InvalidBucketNameException, ErrorResponseException {
+    private int getFilesAmount(String host) throws Exception {
         File traceFile = getProject().file(LEGA_PRIVATE_TMP_TRACE);
         String accessKey = readTrace(traceFile, VAULT_S3_ACCESS_KEY);
         String secretKey = readTrace(traceFile, VAULT_S3_SECRET_KEY);
-        MinioClient minioClient = new MinioClient(String.format("https://%s:9000", host), accessKey, secretKey, true);
+        MinioClient minioClient =
+            new MinioClient(host, 9000, accessKey, secretKey, "us-east-1", true, getOkHttpClient());
         if (!minioClient.bucketExists(VAULT_S3_BUCKET_NAME)) {
             return 0;
         }
-        int size = IterableUtils.size(minioClient.listObjects(VAULT_S3_BUCKET_NAME));
-        return size;
+        return IterableUtils.size(minioClient.listObjects(VAULT_S3_BUCKET_NAME));
     }
 
-    @InputFile
-    public File getRawFile() {
+    private OkHttpClient getOkHttpClient() throws Exception {
+        SSLContext sslContext;
+        TrustManager[] trustManagers;
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keyStore.load(null, null);
+        File certFile = getProject().getParent().file("common/.tmp/ssl/CA.cert");
+        try (FileInputStream fis = new FileInputStream(certFile)) {
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            X509Certificate certificate =
+                (X509Certificate) certificateFactory.generateCertificate(fis);
+            keyStore
+                .setCertificateEntry(certificate.getSubjectX500Principal().getName(), certificate);
+            TrustManagerFactory trustManagerFactory =
+                TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(keyStore);
+            trustManagers = trustManagerFactory.getTrustManagers();
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustManagers, null);
+        }
+        return new OkHttpClient.Builder()
+            .sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustManagers[0])
+            .build();
+    }
+
+    @InputFile public File getRawFile() {
         return getProject().file(".tmp/data.raw");
     }
 
