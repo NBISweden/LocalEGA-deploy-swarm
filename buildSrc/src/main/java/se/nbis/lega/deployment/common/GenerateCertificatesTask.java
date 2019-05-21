04f7f2352cd3da8a767d0ad4beecb4be8bf4fc4e
Dmytro Titov <dmytro.titov@gmail.com>
2019-05-21 20:47:32 +0200

    Fix certificates generation

--- buildSrc/src/main/java/se/nbis/lega/deployment/common/GenerateCertificatesTask.java
+++ buildSrc/src/main/java/se/nbis/lega/deployment/common/GenerateCertificatesTask.java
@@ -4,20 +4,25 @@ import org.apache.sshd.common.config.keys.KeyUtils;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.X500NameBuilder;
 import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.asn1.x509.*;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.bc.BcX509ExtensionUtils;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.cms.CMSSignedDataGenerator;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.internal.Pair;
 
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.GeneralSecurityException;
 import java.security.KeyPair;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.time.LocalDate;
@@ -26,25 +31,25 @@ import java.util.Date;
 
 public class GenerateCertificatesTask extends CommonTask {
 
-    @TaskAction
-    public void run() throws Exception {
+    @TaskAction public void run() throws Exception {
         getProject().file(".tmp/ssl/").mkdirs();
-        X509Certificate root = generateCertificate(null, "CA");
-        generateCertificate(root, "vaultS3");
-        generateCertificate(root, "inboxS3");
-        generateCertificate(root, "cegaMQ");
-        generateCertificate(root, "cegaUsers");
-        generateCertificate(root, "db");
-        generateCertificate(root, "finalize");
-        generateCertificate(root, "inbox");
-        generateCertificate(root, "ingest");
-        generateCertificate(root, "keys");
-        generateCertificate(root, "privateMQ");
-        generateCertificate(root, "publicMQ");
-        generateCertificate(root, "verify");
+        Pair<X509Certificate, KeyPair> root = generateCertificate(null, null, "CA");
+        generateCertificate(root.left, root.right, "vaultS3");
+        generateCertificate(root.left, root.right, "inboxS3");
+        generateCertificate(root.left, root.right, "cegaMQ");
+        generateCertificate(root.left, root.right, "cegaUsers");
+        generateCertificate(root.left, root.right, "db");
+        generateCertificate(root.left, root.right, "finalize");
+        generateCertificate(root.left, root.right, "inbox");
+        generateCertificate(root.left, root.right, "ingest");
+        generateCertificate(root.left, root.right, "keys");
+        generateCertificate(root.left, root.right, "privateMQ");
+        generateCertificate(root.left, root.right, "publicMQ");
+        generateCertificate(root.left, root.right, "verify");
     }
 
-    private X509Certificate generateCertificate(X509Certificate rootCertificate, String service)
+    private Pair<X509Certificate, KeyPair> generateCertificate(X509Certificate rootCertificate,
+        KeyPair parentKeypair, String service)
         throws IOException, GeneralSecurityException, OperatorCreationException {
         KeyPair keyPair = KeyUtils.generateKeyPair("ssh-rsa", 2048);
 
@@ -55,7 +60,7 @@ public class GenerateCertificatesTask extends CommonTask {
 
         X509v3CertificateBuilder builder = rootCertificate == null ?
             new JcaX509v3CertificateBuilder(
-                new X500NameBuilder(BCStyle.INSTANCE).addRDN(BCStyle.CN, service).build(),
+                subject,
                 getSecureRandomSerial(),
                 Date.from(LocalDate.of(2018, 1, 1).atStartOfDay(ZoneOffset.UTC).toInstant()),
                 Date.from(LocalDate.of(2020, 1, 1).atStartOfDay(ZoneOffset.UTC).toInstant()),
@@ -64,9 +69,34 @@ public class GenerateCertificatesTask extends CommonTask {
                 Date.from(LocalDate.of(2018, 1, 1).atStartOfDay(ZoneOffset.UTC).toInstant()),
                 Date.from(LocalDate.of(2020, 1, 1).atStartOfDay(ZoneOffset.UTC).toInstant()),
                 subject, keyPair.getPublic());
+        if (rootCertificate == null) {
+            builder.addExtension(Extension.subjectKeyIdentifier, false,
+                getSubjectKeyId(keyPair.getPublic()));
+            builder.addExtension(Extension.authorityKeyIdentifier, false,
+                getAuthorityKeyId(keyPair.getPublic()));
+            builder.addExtension(Extension.basicConstraints, false, new BasicConstraints(true));
+            builder.addExtension(Extension.keyUsage, true, new KeyUsage(
+                KeyUsage.keyCertSign | KeyUsage.digitalSignature | KeyUsage.keyEncipherment
+                    | KeyUsage.dataEncipherment | KeyUsage.cRLSign));
+            ExtendedKeyUsage extendedKeyUsage = new ExtendedKeyUsage(
+                new KeyPurposeId[] {KeyPurposeId.id_kp_serverAuth, KeyPurposeId.id_kp_clientAuth,
+                    KeyPurposeId.anyExtendedKeyUsage});
+            builder.addExtension(Extension.extendedKeyUsage, true, extendedKeyUsage);
+        } else {
+            builder.addExtension(Extension.subjectKeyIdentifier, false,
+                getSubjectKeyId(keyPair.getPublic()));
+            builder.addExtension(Extension.authorityKeyIdentifier, false,
+                getAuthorityKeyId(parentKeypair.getPublic()));
+            builder.addExtension(Extension.basicConstraints, false, new BasicConstraints(false));
+            builder.addExtension(Extension.keyUsage, true, new KeyUsage(
+                KeyUsage.nonRepudiation | KeyUsage.digitalSignature | KeyUsage.keyEncipherment));
+            ExtendedKeyUsage extendedKeyUsage = new ExtendedKeyUsage(
+                new KeyPurposeId[] {KeyPurposeId.id_kp_serverAuth, KeyPurposeId.id_kp_clientAuth});
+            builder.addExtension(Extension.extendedKeyUsage, true, extendedKeyUsage);
+        }
 
-        ContentSigner signer =
-            new JcaContentSignerBuilder("SHA256withRSA").build(keyPair.getPrivate());
+        ContentSigner signer = new JcaContentSignerBuilder("SHA256withRSA")
+            .build(parentKeypair == null ? keyPair.getPrivate() : parentKeypair.getPrivate());
         X509CertificateHolder holder = builder.build(signer);
 
         JcaX509CertificateConverter converter = new JcaX509CertificateConverter();
@@ -77,7 +107,7 @@ public class GenerateCertificatesTask extends CommonTask {
 
         writePrivateKey(keyPair, getProject().file(".tmp/ssl/" + service + ".key"));
 
-        return certificate;
+        return Pair.of(certificate, keyPair);
     }
 
     private BigInteger getSecureRandomSerial() {
@@ -85,6 +115,16 @@ public class GenerateCertificatesTask extends CommonTask {
         byte[] id = new byte[20];
         random.nextBytes(id);
         return new BigInteger(160, random);
+    }
+
+    private AuthorityKeyIdentifier getAuthorityKeyId(PublicKey pub) {
+        SubjectPublicKeyInfo info = SubjectPublicKeyInfo.getInstance(pub.getEncoded());
+        return new BcX509ExtensionUtils().createAuthorityKeyIdentifier(info);
+    }
+
+    private SubjectKeyIdentifier getSubjectKeyId(PublicKey pub) {
+        SubjectPublicKeyInfo info = SubjectPublicKeyInfo.getInstance(pub.getEncoded());
+        return new BcX509ExtensionUtils().createSubjectKeyIdentifier(info);
     }
 
 }
