172c689e2c54b650a2f0a2de776b11f03a62fdf9
Dmytro Titov <dmytro.titov@gmail.com>
2019-05-23 14:37:43 +0200

    Fix SSL connectivity in VerifyTask

--- buildSrc/src/main/java/se/nbis/lega/deployment/test/VerifyTask.java
+++ buildSrc/src/main/java/se/nbis/lega/deployment/test/VerifyTask.java
@@ -1,16 +1,14 @@
 package se.nbis.lega.deployment.test;
 
-import static se.nbis.lega.deployment.cluster.Machine.LEGA_PRIVATE;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Properties;
+import lombok.extern.slf4j.Slf4j;
 import org.gradle.api.GradleException;
 import org.gradle.api.tasks.TaskAction;
-import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+import static se.nbis.lega.deployment.cluster.Machine.LEGA_PRIVATE;
 
 @Slf4j
 public class VerifyTask extends TestTask {
@@ -29,11 +27,17 @@ public class VerifyTask extends TestTask {
         }
         String port = "5432";
         String db = "lega";
-        String url = String.format("jdbc:postgresql://%s:%s/%s", host, port, db);
+        String ca = getProject().file("common/.tmp/ssl/CA.cert").getAbsolutePath();
+        String cert = getProject().file("common/.tmp/ssl/db.cert").getAbsolutePath();
+        String key = getProject().file("common/.tmp/ssl/db.key").getAbsolutePath();
+        String url = String.format(
+            "jdbc:postgresql://%s:%s/%s?application_name=LocalEGA&sslmode=verify-full&sslcert=%s&sslkey=/%s&sslrootcert=%s",
+            host, port, db, cert, key, ca);
         Properties props = new Properties();
         props.setProperty("user", "lega_in");
-        props.setProperty("password", readTrace(getProject().file(LEGA_PRIVATE_TMP_TRACE), DB_LEGA_IN_PASSWORD));
-        props.setProperty("ssl", "false");
+        props.setProperty("password",
+            readTrace(getProject().file(LEGA_PRIVATE_TMP_TRACE), DB_LEGA_IN_PASSWORD));
+        props.setProperty("ssl", "true");
         try {
             Connection conn = DriverManager.getConnection(url, props);
             String sql = "select status from local_ega.files where id = ?";
@@ -49,4 +53,5 @@ public class VerifyTask extends TestTask {
         }
         System.out.println("Verification completed successfully");
     }
+
 }
